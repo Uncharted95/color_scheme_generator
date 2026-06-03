e840c7abfcb466ad5d17478dea92ae651b5cc086
Alexander A <41549546+Uncharted95@users.noreply.github.com>
2026-06-02 23:05:37 -0400

    Added the final codes.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const btn = document.getElementById("button")
-const colorPicker = document.getElementById("colorSelect")
+const colorDropDown = document.getElementById("colorDropDown")
+const color = document.querySelector('[type="color"')
 
 
-colorPicker.addEventListener("change", function(){
-    fetch("https://www.thecolorapi.com/scheme?mode=analogic")
+btn.addEventListener("click", function(){
+    const url = `https://www.thecolorapi.com/scheme?hex=${color.value.slice(1)}&mode=${colorDropDown.value}&count=5`
+    fetch(url)
     .then(response => response.json())
-    .then(data => )
+    .then(data => {
+        for(let i = 0; i < 5; i++){
+            const colors = document.getElementById(`c${i}`)
+            const hex = document.getElementById(`h${i}`)
+            colors.style.backgroundColor = data.colors[i].hex.value
+            hex.textContent = data.colors[i].hex.value
+        }
+
+    }
+
+    )
 })
